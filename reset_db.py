51d311523cfca96167f8635de305d510167a2928
unknown <wagnerzaparolli15@gmail.com>
2026-02-24 20:30:58 -0300

    Cleanup: Organização da estrutura de pastas para o Render

--- reset_db.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-
-with app.app_context():
-    print("Apagando tabelas antigas...")
-    db.drop_all()
-    print("Criando tabelas novas e atualizadas...")
-    db.create_all()
-    print("Banco de dados resetado com sucesso! Pode rodar o site.")
